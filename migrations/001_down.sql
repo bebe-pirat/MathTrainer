9d8b5b0dec612e5f88ab78dfdfe13f69588eec55
bebe-pirat <pirat.bebe@gmail.com>
2026-03-08 11:34:25 +0300

    project structure is created

---
+++ migrations/001_down.sql
@@ -0,0 +1,23 @@
+DROP TABLE Schools;
+
+DROP TABLE Classes;
+
+DROP TABLE Roles;
+
+DROP TABLE Users;
+
+DROP TABLE Achievements;
+
+DROP TABLE Achievements_of_students;
+
+DROP TABLE Equation_types;
+
+DROP TABLE Levels;
+
+DROP TABLE Equations;
+
+DROP TABLE Student_progress;
+
+DROP TABLE Level_equation_type;
+
+DROP TABLE Theory;
---
+++ migrations/001_up.sql
@@ -0,0 +1,171 @@
+CREATE TABLE Schools (
+	Id INT SERIAL PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL,
+	Address VARCHAR(200) NOT NULL,
+	Created_at DATETIME2(0) NOT NULL
+);
+
+CREATE TABLE Classes (
+	Id INT SERIAL PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL,
+	Grade INT CHECK (Grade > 0 AND Grade < 5) NOT NULL,
+	School_Id INT NOT NULL,
+	Created_at DATETIME2(0) NOT NULL,
+	CONSTRAINT FK_Classes_School_id  FOREIGN KEY(School_Id) REFERENCES Schools(Id) ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE TABLE Roles (
+	Id INT SERIAL PRIMARY KEY,
+	Name VARCHAR(100) UNIQUE,
+);
+
+CREATE TABLE Users (
+	Id INT SERIAL PRIMARY KEY,
+	Email VARCHAR(100) NULL UNIQUE,
+	Login VARCHAR(100) NOT NULL UNIQUE,
+	Password_hash VARCHAR(100) NOT NULL, 
+	Role_Id INT NULL,
+	Blocked BIT NOT NULL,
+	FullName VARCHAR(200) NOT NULL,
+	Class_Id INT NULL,
+	CONSTRAINT FK_Users_Class_Id FOREIGN KEY(Class_Id) REFERENCES Classes(Id) ON UPDATE CASCADE ON DELETE NO ACTION,
+	CONSTRAINT FK_Users_Role_Id FOREIGN KEY(Role_Id) REFERENCES Roles(Id) ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE TABLE Achievements (
+	Id INT SERIAL PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL,
+	Description VARCHAR(MAX) NULL
+);
+
+CREATE TABLE Achievements_of_students (
+	Student_Id INT NOT NULL,
+	Achievement_Id INT NOT NULL,
+	Got_at DATETIME2(0) NOT NULL,
+	CONSTRAINT PK_Achievements_of_students PRIMARY KEY(Student_Id, Achievement_Id),
+	CONSTRAINT FK_Achievements_of_students_Student_Id  FOREIGN KEY(Student_Id) REFERENCES Users(Id) ON UPDATE CASCADE ON DELETE NO ACTION,
+	CONSTRAINT FK_Achievements_of_students_Achievement_Id  FOREIGN KEY(Achievement_Id) REFERENCES Achievements(Id) ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE TABLE Equation_types (
+	Id INT SERIAL PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL,
+	Description VARCHAR(MAX) NULL
+);
+
+CREATE TABLE Levels (
+	Id INT SERIAL PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL,
+	Test_level BIT NOT NULL,
+	Difficulty INTEGER NOT NULL
+);
+
+CREATE TABLE Equations (
+	Id INT SERIAL PRIMARY KEY,
+	Expression VARCHAR(100) NOT NULL,
+	Correct_answer INT NOT NULL,
+	Equaition_type_Id INT NOT NULL,
+	Difficulty INTEGER NOT NULL,
+	CONSTRAINT FK_Equations_Equaition_type_Id  FOREIGN KEY(Equaition_type_Id) REFERENCES Equation_types(Id) ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE TABLE Student_progress (
+	Id INT SERIAL PRIMARY KEY,
+	Student_Id INT NOT NULL,
+	Level_id INT NOT NULL,
+	Count_stars INT,
+	Finished_at DATETIME2(0) NULL,
+	CONSTRAINT FK_Student_progress_Student_Id  FOREIGN KEY(Student_Id) REFERENCES Users(Id) ON UPDATE CASCADE ON DELETE NO ACTION,
+	CONSTRAINT FK_Student_progress_Level_Id  FOREIGN KEY(Level_Id) REFERENCES Levels(Id) ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE TABLE Level_equation_type (
+	Level_Id INT NOT NULL,
+	Equation_type_Id INT NOT NULL,
+	CONSTRAINT PK_Level_equation_type PRIMARY KEY(Level_Id, Equation_type_Id),
+	CONSTRAINT FK_Level_equation_type_Equation_type_Id  FOREIGN KEY(Equation_type_Id) REFERENCES Equation_types(Id) ON UPDATE CASCADE ON DELETE NO ACTION,
+	CONSTRAINT FK_Level_equation_type_Level_Id  FOREIGN KEY(Level_Id) REFERENCES Levels(Id) ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE TABLE Theory (
+	Id INT SERIAL PRIMARY KEY,
+	Equation_type_Id INT NOT NULL,
+	Name VARCHAR(100),
+	Content VARCHAR(MAX),
+	CONSTRAINT FK_Theory_Equation_type_Id  FOREIGN KEY(Equation_type_Id) REFERENCES Equation_types(Id) ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+INSERT INTO Schools(Name, Address, Created_at)
+VALUES ('Общеобразовательная школа «Средняя общеобразовательная школа № 322»', 'Санкт-Петербург, улица Олеко Дундича, 38, корп. 3, метро «Дунайская»', GETDATE()),
+('ГБОУ СОШ № 555 с углубленным изучением английского языка «Белогорье»','Санкт-Петербург, Комендантский проспект, 17, корп. 3, метро «Комендантский проспект»', GETDATE()),
+('ГБОУ Средняя общеобразовательная школа № 111 с углубленным изучением немецкого языка', 'Санкт-Петербург, улица Фаворского, дом 16, литер А', GETDATE());
+
+
+INSERT INTO Classes	(Name, Grade, School_Id, Created_at)
+VALUES ('3А', 3, 1, GETDATE()),
+('2А', 2, 1, GETDATE()),
+('2Б', 2, 1, GETDATE());
+
+INSERT INTO Roles(Name)
+VALUES ('Student'),
+('Administrator'),
+('Teacher'),
+('Head');
+
+INSERT INTO Users (Email, Login, Password_hash, Role_Id, Blocked, FullName, Class_Id)
+VALUES ('student1@gmail.com', 'student1', '$2a$10$Qtc61.Gn4rXMbajz/iBYV.kV4Tv4jQIB50sBcfWX6s6CtHbXsuGS2', 1, 0, 'Семенов Семен Семенович', 3),
+('student2@gmail.com', 'student2', '$2a$10$hTAYihicrLbDYqqPoIXFBus5EqXbE/mwm5tXQlljp3ECREHVAeECG', 1, 0, 'Николаев Николай Николаевич', 3),
+('admin@gmail.com', 'admin', '$2a$10$ToSI7ka8vIXEMwPhobZZkuAqu3NTlYFQj3.eE7oXnqxZtl7twak4C', 2, 0, 'Дарьина Дарья Дарьевна', NULL),
+('teacher1@gmail.com', 'teacher1', '$2a$10$alzWzsvxHWvdsihcWfu2rezwplu/hKrVGLKhyVhiKjaHA4zGs44OO', 3, 0, 'Марьина Марья Маровна', 3);
+
+INSERT INTO Achievements (Name, Description)
+VALUES ('Бронзовый кубок', 'Мог бы и лучше сделать)'),
+('Серебряный кубок', 'А почему не первое место?'),
+('Золотой кубок', 'Ты чемпион, выше всех, знай это!');
+
+INSERT INTO Achievements_of_students (Student_Id, Achievement_Id, Got_at)
+VALUES (1, 1, GETDATE()),
+(2, 1, GETDATE()),
+(1, 2, GETDATE());
+
+INSERT INTO Equation_types(Name, Description)
+VALUES ('Простейшие уравнения на сложение', 'Нахождение неизвестного слагаемого'),
+('Простейшие уравнения на вычитание', 'Нахождение вычитаемого и уменьшаемого'),
+('Простейшие уравнения на умножение', 'Нахождение множителя');
+INSERT INTO Equation_types(Name, Description)
+VALUES 
+('Простейшие уравнения на деление', 'Нахождение делителя'),
+('Составные уравнения', 'Уравнения в несколько действий');
+INSERT INTO Levels (Name, Test_level, Difficulty)
+VALUES
+('Уровень 1', 0, 1),
+('Уровень 2', 0, 2),
+('Итоговый тест', 1, 3);
+INSERT INTO Equations (Expression, Correct_answer, Equaition_type_Id, Difficulty)
+VALUES
+('x + 3 = 7', 4, 1, 1),
+('x - 5 = 2', 7, 2, 1),
+('3 * x = 12', 4, 3, 2),
+('x / 2 = 5', 10, 4, 2),
+('2 * x + 3 = 11', 4, 5, 3);
+INSERT INTO Student_progress (Student_Id, Level_Id, Count_stars, Finished_at)
+VALUES
+(1, 1, 3, GETDATE()),
+(1, 2, 2, GETDATE()),
+(2, 1, 1, GETDATE());
+INSERT INTO Level_equation_type (Level_Id, Equation_type_Id)
+VALUES
+(1, 1),
+(1, 2),
+(2, 3),
+(3, 5);
+INSERT INTO Theory (Equation_type_Id, Name, Content)
+VALUES
+(1, 'Как находить неизвестное слагаемое', 
+ 'Чтобы найти неизвестное слагаемое, нужно из суммы вычесть известное слагаемое.'),
+
+(2, 'Как находить уменьшаемое и вычитаемое', 
+ 'Чтобы найти уменьшаемое, нужно к разности прибавить вычитаемое.'),
+
+(3, 'Как находить неизвестный множитель', 
+ 'Чтобы найти множитель, нужно произведение разделить на известный множитель.');
