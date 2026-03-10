d23802623f4050c243f927c1ee71d21d65ad9fb6
bebe-pirat <pirat.bebe@gmail.com>
2026-03-10 13:27:42 +0300

    first version of main.go with no server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"MathTrainer/internal/database"
+	"log/slog"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	err := godotenv.Load()
+	if err != nil {
+		logger.Info("unable to load .env")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	connectionString := os.Getenv("DATABASE_URL")
+	if connectionString == "" {
+		logger.Error("no connection string")
+		return
+	}
+
+	db, err := database.OpenDB(connectionString)
+	if err != nil {
+		logger.Error("failed to connect to database", "error", err)
+	}
+	defer db.Close()
+
+	// server = http.Server{}
+}
