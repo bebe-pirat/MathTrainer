57d1b559d7ed40d7dd9e284ce17f75aa849c652d
bebe-pirat <pirat.bebe@gmail.com>
2026-03-16 16:02:34 +0300

    stats service is done

---
+++ internal/service/stats_service.go
@@ -0,0 +1,173 @@
+package service
+
+import (
+	"MathTrainer/internal/model"
+	"MathTrainer/internal/repository"
+	"context"
+)
+
+type StatsService interface {
+	GetSchoolStats(ctx context.Context, schoolId int) (*model.SchoolStats, error)
+	GetClassStats(ctx context.Context, classId int) (*model.ClassStats, error)
+	GetStudentStats(ctx context.Context, studentId int) (*model.StudentStats, error)
+}
+
+type StatsServiceStruct struct {
+	classRepo    repository.ClassRepository
+	schoolRepo   repository.SchoolRepository
+	studentRepo  repository.UserRepository
+	attemptRepo  repository.EquationAttemptsRepository
+	progressRepo repository.StudentProgressRepository
+	achievRepo   repository.AchievementOfStudentRepository
+}
+
+func NewStatStatsServiceStruct(classRepo repository.ClassRepository,
+	schoolRepo repository.SchoolRepository,
+	studentRepo repository.UserRepository,
+	attemptsRepo repository.EquationAttemptsRepository,
+	progressRepo repository.StudentProgressRepository,
+	achievRepo repository.AchievementOfStudentRepository) *StatsServiceStruct {
+	return &StatsServiceStruct{
+		classRepo:    classRepo,
+		schoolRepo:   schoolRepo,
+		studentRepo:  studentRepo,
+		attemptRepo:  attemptsRepo,
+		progressRepo: progressRepo,
+		achievRepo:   achievRepo,
+	}
+}
+
+func (s *StatsServiceStruct) GetSchoolStats(ctx context.Context, schoolId int) (*model.SchoolStats, error) {
+	countStudents, err := s.studentRepo.GetTotalStudentBySchoolId(ctx, schoolId)
+	if err != nil {
+		return nil, err
+	}
+
+	classes, err := s.classRepo.GetClassesBySchoolId(ctx, schoolId)
+	if err != nil {
+		return nil, err
+	}
+
+	totalCount, err := s.attemptRepo.GetTotalAttemptsBySchoolId(ctx, schoolId)
+	if err != nil {
+		return nil, err
+	}
+
+	wrongCount, err := s.attemptRepo.GetWrongAnswersBySchoolId(ctx, schoolId)
+	if err != nil {
+		return nil, err
+	}
+
+	accuracy := float32(totalCount) / float32(wrongCount) * 100.0
+
+	equationTypes, err := s.attemptRepo.GetEquationTypeAccuracyBySchoolId(ctx, schoolId)
+	if err != nil {
+		return nil, err
+	}
+
+	classStats, err := s.attemptRepo.GetClassesAccuracyBySchoolId(ctx, schoolId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.SchoolStats{
+		StudentsCount:       countStudents,
+		ClassesCount:        len(classes),
+		TotalEquationSolved: totalCount,
+		WrongAnswers:        wrongCount,
+		Accuracy:            accuracy,
+		EquationTypes:       equationTypes,
+		Classes:             classStats,
+	}, nil
+}
+
+func (s *StatsServiceStruct) GetClassStats(ctx context.Context, classId int) (*model.ClassStats, error) {
+	students, err := s.attemptRepo.GetStudentsShortStatsByClassId(ctx, classId)
+	if err != nil {
+		return nil, err
+	}
+
+	studentCount := len(students)
+
+	totalCount, err := s.attemptRepo.GetTotalAttemptsByClassId(ctx, classId)
+	if err != nil {
+		return nil, err
+	}
+
+	wrongCount, err := s.attemptRepo.GetWrongAnswersByClassId(ctx, classId)
+	if err != nil {
+		return nil, err
+	}
+
+	correctCount := totalCount - wrongCount
+	accuracy := float32(correctCount) / float32(totalCount) * 100.0
+
+	equationTypes, err := s.attemptRepo.GetEquationTypeAccuracyByClassId(ctx, classId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.ClassStats{
+		StudentsCount:  studentCount,
+		TotalAttempts:  totalCount,
+		WrongAnswers:   wrongCount,
+		CorrectAnswers: correctCount,
+		Accuracy:       accuracy,
+		EquationTypes:  equationTypes,
+		Students:       students,
+	}, nil
+}
+
+func (s *StatsServiceStruct) GetStudentStats(ctx context.Context, studentId int) (*model.StudentStats, error) {
+	totalCount, err := s.attemptRepo.GetAllStats(ctx, studentId)
+	if err != nil {
+		return nil, err
+	}
+
+	wrongCount, err := s.attemptRepo.GetErrorStats(ctx, studentId)
+	if err != nil {
+		return nil, err
+	}
+
+	correctCount := totalCount - wrongCount
+	accuracy := float32(correctCount) / float32(totalCount) * 100.0
+
+	complitedLevels, err := s.progressRepo.GetComplitedLevels(ctx, studentId)
+	if err != nil {
+		return nil, err
+	}
+
+	starsCount, err := s.progressRepo.GetTotalStars(ctx, studentId)
+	if err != nil {
+		return nil, err
+	}
+
+	achievements, err := s.achievRepo.GetAchievementOfStudentsByStudentId(ctx, studentId)
+	if err != nil {
+		return nil, err
+	}
+
+	equationTypes, err := s.attemptRepo.GetExtendedEquationTypeStats(ctx, studentId)
+	if err != nil {
+		return nil, err
+	}
+
+	weakTopics := make([]string, 0)
+	for _, value := range equationTypes {
+		if value.Accuracy < 50.0 {
+			weakTopics = append(weakTopics, value.Type)
+		}
+	}
+
+	return &model.StudentStats{
+		TotalAttempts:   totalCount,
+		CorrectAnswers:  correctCount,
+		WrongAnswers:    wrongCount,
+		Accuracy:        accuracy,
+		LevelsCompleted: complitedLevels,
+		StarsTotal:      starsCount,
+		EquationTypes:   equationTypes,
+		Achievements:    achievements,
+		WeakTopics:      weakTopics,
+	}, nil
+}
