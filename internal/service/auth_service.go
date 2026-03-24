1256292cb0d28638b0a3328e0f22c259e261d638
bebe-pirat <pirat.bebe@gmail.com>
2026-03-24 14:22:24 +0300

    auth service is done

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -14,27 +14,39 @@ type AuthService interface {
 }
 
 type AuthServiceStruct struct {
-	userRepo repository.UserRepository
+	userRepo    repository.UserRepository
+	sessionRepo repository.SessionRepository
 }
 
-func NewAuthServiceStruct(userRepo repository.UserRepository) *AuthServiceStruct {
+func NewAuthServiceStruct(userRepo repository.UserRepository, sessionRepo repository.SessionRepository) *AuthServiceStruct {
 	return &AuthServiceStruct{
-		userRepo: userRepo,
+		userRepo:    userRepo,
+		sessionRepo: sessionRepo,
 	}
 }
 
-func (s *AuthServiceStruct) Login(ctx context.Context, login string, password string) (int, error)
+// пока что так
+func (s *AuthServiceStruct) Login(ctx context.Context, login string, password string) (int, error) {
+	userId, err := s.userRepo.UserExists(ctx, login, password)
+	if err != nil {
+		return 0, err
+	}
 
-// пока что оставим это
-// разберись с JWT и созданием сессии + куки
+	sessionId, err := s.sessionRepo.CreateSession(ctx, userId, time.Now().Add(time.Hour*24))
+	if err != nil {
+		return 0, err
+	}
 
-func (s *AuthServiceStruct) Logout(ctx context.Context)
+	return sessionId, err
+}
 
-// подумать, что тут сделать
-/* сделать 3 миграцию, где создаться таблица session
-при logout надо удалять сессию из БД, из куков и что-то сделать с JWT(?)
-тоже снести скорее всего
-*/
+func (s *AuthServiceStruct) Logout(ctx context.Context, sessionId int) error {
+	if sessionId <= 0 {
+		return errors.New("require session id")
+	}
+
+	return s.sessionRepo.DeleteSession(ctx, sessionId)
+}
 
 func (s *AuthServiceStruct) UpdateLastLoginUser(ctx context.Context, id int, lastLogin time.Time) error {
 	if id <= 0 {
